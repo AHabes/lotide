274e1c5540bdc58ab22fa9b3dbb129ba03a2e028
Abdullah Habes <abdulla096@hotmail.com>
2021-10-26 23:33:58 -0400

    implement flatten

---
+++ flatten.js
@@ -0,0 +1,14 @@
+const flatten = arr => {
+  let flattened = [];
+  for (let element of arr) {
+    if (Array.isArray(element)) {
+      for (let subElement of element)
+        flattened.push(subElement);
+    } else
+      flattened.push(element);
+  }
+  return flattened;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6])
+console.log(flatten([[1, 2], [3, 4], [6], ["5"], ['a', 'b']])); // => [1, 2, 3, 4, 5, 6])
