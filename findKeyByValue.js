64fc88bf816fff4718c6918f522df376a29e9b8d
Abdullah Habes <abdulla096@hotmail.com>
2021-10-27 18:15:11 -0400

    implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`${String.fromCodePoint(0x2705)} : ${actual} === ${expected}`);
+  else
+    console.log(`${String.fromCodePoint(0x274C)} : ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (obj, value) => {
+  for (let element in obj) {
+    if (obj[element] === value)
+      return element;
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const persons = {
+  firstName: "John",
+  age: 21,
+  hobbies: "football",
+  givenName: "John",
+  name: "John",
+  bestSports: "football"
+};
+
+assertEqual(findKeyByValue(persons, "John"), "firstName");
+assertEqual(findKeyByValue(persons, "football"), "hobbies");
+assertEqual(findKeyByValue(persons, 21), "age");
