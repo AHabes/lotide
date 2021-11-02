7faf4be7bb2d21545dd1ef17631564f1b326efc3
Abdullah Habes <abdulla096@hotmail.com>
2021-11-01 21:52:16 -0400

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,4 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length)
-    return false;
-  else {
-    for (let i = 0; i < arr1.length; i++)
-      if (arr1[i] !== arr2[i])
-        return false;
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2))
@@ -16,11 +7,4 @@ const assertArraysEqual = (arr1, arr2) => {
     console.log(`${String.fromCodePoint(0x274C)} : ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
 };
 
-
-// TEST CODE
-assertArraysEqual(["Lighthouse Labs"], ["Bootcamp"]);
-assertArraysEqual(["Bootcamp"], ["Bootcamp"]);
-assertArraysEqual([1], [0]);
-assertArraysEqual(['ABCD'], ['ABCD', 'AAAA']);
-assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
-assertArraysEqual([1, 2, 3, 4, 5], [1, 1, 1, 1, 1]);
+module.exports = assertArraysEqual;
