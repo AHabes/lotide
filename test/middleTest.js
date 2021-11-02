78a929e1cbb2acb8e2aef8011f2f156ef089649b
Abdullah Habes <abdulla096@hotmail.com>
2021-11-01 23:21:09 -0400

    added M&C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,42 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
+const chai = require('chai');
+const assert = chai.assert;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4,]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe('#middle', () => {
+  it('should return [] for []', () => {
+    const expectedOutput = [];
+    const actualOutput = middle([1]);
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should return [] for middle([1, 2])', () => {
+    const expectedOutput = [];
+    const actualOutput = middle([1, 2]);
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should return [2] for middle([1, 2, 3])', () => {
+    const expectedOutput = [2];
+    const actualOutput = middle([1, 2, 3]);
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should return [3] for middle([1, 2, 3, 4, 5])', () => {
+    const expectedOutput = [3];
+    const actualOutput = middle([1, 2, 3, 4, 5]);
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should return [2,3] for middle([1, 2, 3, 4,])', () => {
+    const expectedOutput = [2, 3];
+    const actualOutput = middle([1, 2, 3, 4,]);
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should return [3,4] for middle([1, 2, 3, 4, 5, 6])', () => {
+    const expectedOutput = [3, 4];
+    const actualOutput = middle([1, 2, 3, 4, 5, 6]);
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,37 +1,57 @@
 const assertEqual = require('../assertEqual');
 const tail = require("../tail");
 
+const chai = require('chai');
+const assert = chai.assert;
 
+describe('#tail', () => {
+  it('should return length of 2 for ["Hello", "Lighthouse", "Labs"]', () => {
+    let arr = ["Hello", "Lighthouse", "Labs"];
+    const result = tail(arr);
+    assert.strictEqual(result.length, 2);
+  });
 
-// Test Case 1: Check the returned array elements
-let arr = ["Hello", "Lighthouse", "Labs"];
-const result = tail(arr);
-assertEqual(result.length, 2); // ensure we get back two elements
+  it('should return length of ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    let arr = ["Hello", "Lighthouse", "Labs"];
+    const result = tail(arr);
+    console.log(result);
+    arr.slice(1).forEach((element, index) => {
+      assert.strictEqual(element, result[index]);
+    });
+  });
 
-arr.slice(1).forEach((element, index) => {
-  assertEqual(element, result[index]);
+  it('should return length of 3 for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(words.length, 3);
+  });
+
+  it('should return length of ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    let wordsRes = tail(words);
+    const expectedOutput = ["Lighthouse", "Labs"];
+    assert.deepEqual(wordsRes, expectedOutput);
+  });
+
+  it('should return length of 0 for []', () => {
+    const words_ = [];
+    let wordsRes_ = tail(words_);
+    assert.strictEqual(wordsRes_.length, 0);
+  });
+
+  it('should return length of 0 for [3]', () => {
+    const words__ = [3];
+    let wordsRes__ = tail(words__);
+    assertEqual(wordsRes__.length, 0);
+  });
+
+  it('should return undefined for "[3]"', () => {
+    const wordString = "[3]";
+    let wordStringRes = tail(wordString);
+    assert.strictEqual(wordStringRes, undefined);
+  });
 });
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-
-let wordsRes = tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-words.slice(1).forEach((element, index) => {
-  assertEqual(element, wordsRes[index]);
-});
 
 
-const words_ = [];
-let wordsRes_ = tail(words_);
-assertEqual(wordsRes_.length, 0);
-
-
-const words__ = [3];
-let wordsRes__ = tail(words__);
-assertEqual(wordsRes__.length, 0);
-
-const wordString = "[3]";
-let wordStringRes = tail(wordString);
-assertEqual(wordStringRes, 4);
 
