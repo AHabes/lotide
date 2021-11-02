fe480ae3c50437e512070ec9544b4b129c4696d0
Abdullah Habes <abdulla096@hotmail.com>
2021-11-01 21:48:04 -0400

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    console.log(`${String.fromCodePoint(0x2705)} : ${actual} === ${expected}`);
-  else
-    console.log(`${String.fromCodePoint(0x274C)} : ${actual} !== ${expected}`);
-};
-
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length)
     return false;
@@ -17,10 +9,4 @@ const eqArrays = (arr1, arr2) => {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], []), false); // => false
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], []), false); // => false
+module.exports = eqArrays;
